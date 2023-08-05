505160eb35c06b70a67ee727307f07a6d6a32615
Esmael Pinho <esma.pinho@gmail.com>
2023-08-05 13:55:48 +0100

    added video area to index.html on script.js along with jquery library script

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,48 +1,60 @@
 
+
 document.addEventListener("DOMContentLoaded", function () {
-  const playButton = document.getElementById("play-button");
+  let playButton = document.getElementById("play-button");
+  
 
   function goToLevels() {
     window.location.href = "levels.html";
   }
-
-  playButton.addEventListener("click", goToLevels);
-
-
-  function randomColor() {
-    return (
-      "rgba(" +
-      Math.round(Math.random() * 250) +
-      "," +
-      Math.round(Math.random() * 250) +
-      "," +
-      Math.round(Math.random() * 250) +
-      "," +
-      Math.ceil(Math.random() * 10) / 10 +
-      ")"
-    );
-  }
-
-  function Bubbles() {
-
-      const section = document.querySelector('section');
-      const createBubble = document.createElement('span');
-      var size = Math.random() * 90;
-
-      createBubble.style.background = randomColor();
-      createBubble.style.width = 40 + size + 'px';
-      createBubble.style.height = 40 + size + 'px';
-      createBubble.style.left = Math.random() * innerWidth + 'px';
-      section.appendChild(createBubble);
-
-      setTimeout(() => {
-          createBubble.remove()
-      },8000)
-
-  }
-  setInterval(Bubbles,100);
+  playButton.addEventListener("click", goToLevels); // play button links to levels page
 
 });
 
+// creates random colors for the bubbles
+function randomColor() {
+  return (
+    "rgba(" +
+    Math.round(Math.random() * 250) +
+    "," +
+    Math.round(Math.random() * 250) +
+    "," +
+    Math.round(Math.random() * 250) +
+    "," +
+    Math.ceil(Math.random() * 10) / 10 +
+    ")"
+  );
+}
+
+function Bubbles() {
+
+    const section = document.querySelector('section');
+    const createBubble = document.createElement('span');
+    var size = Math.random() * 90;
+
+    createBubble.style.background = randomColor();
+    createBubble.style.width = 40 + size + 'px';
+    createBubble.style.height = 40 + size + 'px';
+    createBubble.style.left = Math.random() * innerWidth + 'px';
+    section.appendChild(createBubble);
+
+    setTimeout(() => {
+        createBubble.remove()
+    },8000)
+
+}
+  setInterval(Bubbles,100);
+
+// adds demo video after clicking demo-button 
+function demoWindow() {
+  let demo = document.getElementById('demo-window');
+  demo.innerHTML = `<video width="500" controls>
+    <source src="./assets/video/video.mp4" type="video/mp4">
+    Your browser does not support HTML5 video.
+    </video>`;
+};
+
+
+  
 
 
