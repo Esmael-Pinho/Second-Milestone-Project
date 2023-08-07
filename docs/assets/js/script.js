ab9458b102b0bece0860a65d9f8da425dfb2d3ea
Esmael Pinho <esma.pinho@gmail.com>
2023-08-07 12:32:08 +0100

    added open and close of modal-window on script

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,6 +1,7 @@
 
 
 document.addEventListener("DOMContentLoaded", function () {
+
   let playButton = document.getElementById("play-button");
   
 
@@ -9,50 +10,52 @@ document.addEventListener("DOMContentLoaded", function () {
   }
   playButton.addEventListener("click", goToLevels); // play button links to levels page
 
-});
 
-// creates random colors for the bubbles
-function randomColor() {
-  return (
-    "rgba(" +
-    Math.round(Math.random() * 250) +
-    "," +
-    Math.round(Math.random() * 250) +
-    "," +
-    Math.round(Math.random() * 250) +
-    "," +
-    Math.ceil(Math.random() * 10) / 10 +
-    ")"
-  );
-}
+  // creates random colors for the bubbles 
+  function randomColor() {
+    return (
+      "rgba(" +
+      Math.round(Math.random() * 250) +
+      "," +
+      Math.round(Math.random() * 250) +
+      "," +
+      Math.round(Math.random() * 250) +
+      "," +
+      Math.ceil(Math.random() * 10) / 10 +
+      ")"
+    );
+  }
 
-function Bubbles() {
+  function Bubbles() {
 
-    const section = document.querySelector('section');
-    const createBubble = document.createElement('span');
-    var size = Math.random() * 90;
+      const section = document.querySelector('section');
+      const createBubble = document.createElement('span');
+      var size = Math.random() * 90;
 
-    createBubble.style.background = randomColor();
-    createBubble.style.width = 40 + size + 'px';
-    createBubble.style.height = 40 + size + 'px';
-    createBubble.style.left = Math.random() * innerWidth + 'px';
-    section.appendChild(createBubble);
+      createBubble.style.background = randomColor();
+      createBubble.style.width = 40 + size + 'px';
+      createBubble.style.height = 40 + size + 'px';
+      createBubble.style.left = Math.random() * innerWidth + 'px';
+      section.appendChild(createBubble);
 
-    setTimeout(() => {
-        createBubble.remove()
-    },8000)
+      setTimeout(() => {
+          createBubble.remove()
+      },8000)
 
-}
+  }
   setInterval(Bubbles,100);
 
+ });
 
-// links each button to game.html 
-const buttons = document.querySelectorAll("#easy, #medium, #hard");
-  buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        window.location.href = "game.html";
-    });
-  });
+ // links each button to game.html 
+ const buttons = document.querySelectorAll("#easy, #medium, #hard");
+ buttons.forEach(button => {
+   button.addEventListener("click", () => {
+       window.location.href = "game.html";
+   });
+ });
+
+
 
 
 $(document).ready(function() {
@@ -62,6 +65,16 @@ $(document).ready(function() {
   $("#demo-button").click(function() {
     $("video").toggle();
     $(".hide").toggle();
+  });
+
+
+  // modal open after click feedback button
+  $(".message-btn").click(function() {
+    $("#modalContact").show();
+  });
+  // modal close after press X (close) button
+  $(".close-btn").click(function() {
+    $("#modalContact").hide();
   });
 
 });
